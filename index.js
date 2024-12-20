8cad7fd5925d7147fd7b0c2076b6c4fc4b030c5c
Vicheka <pich.vicheka@institute.pse.ngo>
2024-12-20 16:15:07 +0700

    do test

--- index.js
+++ index.js
@@ -3,25 +3,33 @@
 
 // Task 1: Comment Your JavaScript Code
 // Write a single-line comment below this line:
-
+var myName;
+myName="vicheka";
 // Task 2: Declare JavaScript Variables
-// Declare a variable named `myName`:
-
+// Dec lare a variable named `myName`:
+var myName ="myName";
 // Task 3: Storing Values with the Assignment Operator
 // Assign the value "JavaScript" to a variable named `myLanguage`:
-
+var myLanguage =" myLanguage"
 // Task 4: Assigning the Value of One Variable to Another
 // Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`:
-
+var a = 5
+var b = 6
 // Task 5: Initializing Variables with the Assignment Operator
 // Declare and initialize a variable named `myAge` with the value 25:
-
+var myAge;
+myAge = 25;
 // Task 6: Declare String Variables
 // Declare a string variable named `greeting` and set it to "Hello, World!":
 
 // Task 7: Understanding Uninitialized Variables
 // Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them:
-
+var x;
+x = 1;
+var y;
+y = 2;
+var z;
+z = 3;
 // Task 8: Understanding Case Sensitivity in Variables
 // Fix the variable names to follow case sensitivity:
 // let StUdEnTnAmE = "John";
